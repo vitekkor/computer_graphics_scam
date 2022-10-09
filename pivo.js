50d84d832f81afd4674bf26705ccc027c3b30954
vitekkor <viktor.korotkikh@gmail.com>
2022-10-09 18:29:08 +0300

    add sbp payment method

--- pivo.js
+++ pivo.js
@@ -17,17 +17,14 @@ setTimeout(() => {
     let p = document.createElement("p");
     p.innerHTML = "Вот тут ниже qr-код, по которому ты можешь перевести мне деньги и поддержать разработку данного ПО. " +
         "Я потратил немало времени, чтобы ты мог легко списывать этот курс. " +
-        "Так что надеюсь ты отблагодаришь меня за мой труд."
+        "Так что надеюсь ты отблагодаришь меня за мой труд.";
+    let pSBP = document.createElement("p");
+    pSBP.innerHTML = "P.S. также можно через СБП +79117290892 (банк Санкт-Петербург)"
     let p2 = document.createElement("p");
     let img = document.createElement("img")
     img.src = "https://sun9-11.userapi.com/impf/c851420/v851420471/1f0336/w8lFMaioIzA.jpg?size=604x593&quality=96&sign=73f83ecb07f9d3eef70fe116e79114ab&type=album"
     img.height = 150
-    let img2 = document.createElement("img")
-    img2.src = "https://sun9-61.userapi.com/impg/6cVyhEfxkHZjo0rv53Xkpp99j3zecmOGgKn0pQ/MtqLkVQXYmY.jpg?size=810x1080&quality=96&sign=2dc600c43eab3899971e9179a40c17d3&type=album"
-    img2.height = 150
-    img2.style.paddingLeft = "10px"
     p2.appendChild(img)
-    p2.appendChild(img2)
     let close = document.createElement("a")
     close.href = "#"
     close.className = "modal-close"
@@ -38,7 +35,8 @@ setTimeout(() => {
 
     s.appendChild(title);
     s.appendChild(p);
-    s.appendChild(p2)
+    s.appendChild(p2);
+    s.appendChild(pSBP);
     s.appendChild(qr);
     s.appendChild(close)
 
